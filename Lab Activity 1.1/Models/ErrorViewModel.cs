7ad50d13b91621d728006f940073768384ebe911
unknown <UU639@ccs.local>
2024-02-24 06:22:06 +0800

    first lab activity

---
+++ Lab Activity 1.1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab_Activity_1._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

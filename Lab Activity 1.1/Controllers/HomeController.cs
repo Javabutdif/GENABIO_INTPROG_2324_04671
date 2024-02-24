55e53a36a9628a7dbc5062d5247446f9289cbd28
Anton James Genabio <jamesgenabio31@gmail.com>
2024-02-24 17:26:35 +0800

    semi finish needs modify later on

--- Lab Activity 1.1/Controllers/HomeController.cs
+++ Lab Activity 1.1/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace Lab_Activity_1._1.Controllers
         {
             return View();
         }
+        public IActionResult slime()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
